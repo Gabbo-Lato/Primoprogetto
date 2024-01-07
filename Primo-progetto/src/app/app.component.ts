c40e2eebb66629d0be23c9bb1f7f1e5b4db9932a
Gabbo La Torre <la.torre.gabriele@iisgalvanimi.edu.it>
2024-01-07 20:18:26 +0000

    first commit

---
+++ Primo-progetto/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Primo-progetto';
+  listaAnimali = [
+    { id: 1, name: 'Cavallo' },
+    { id: 2, name: 'Scimmia' },
+    { id: 3, name: 'Papera' },
+    { id: 4, name: 'Delfino' },
+  ];
+
+}
